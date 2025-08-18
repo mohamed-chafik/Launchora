8a0b41725a0cdd8f60c77485e0d6ddbd2f0640b6
mohamed-chafik <mchafik2006@gmail.com>
2025-08-18 17:54:20 +0100

    finish structure

--- src/app/description.tsx
+++ src/app/description.tsx
@@ -1,12 +1,13 @@
 import Data from '../components/data.tsx';
 import Thumb from '../components/thumb.tsx'
-import Agency from '../components/agency.tsx'
+
 export default function Description(){
   return(
   <>
   <div className='w-full h-full flex'>
    <Data/>
   <Thumb/>
+
       </div>
     </>
   )
