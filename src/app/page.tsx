d64f5f8142f4480a7399bda375e01d9b6f4fbb8b
mohamed-chafik <mchafik2006@gmail.com>
2025-08-29 16:04:47 +0100

    fetching after run out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,65 +5,35 @@ import Countdown from '../components/countdown.tsx';
 import Icon from '../components/button.tsx';
 import Description from './description.tsx';
 import { useState, useEffect } from 'react';
-import {nextLaunch} from '../lib/utils.ts';
+import {NextLaunch, FetchLaunch, Launch} from '../lib/utils.ts';
+
 export default function Home() {
-  const [nextLaunch, setNextLaunch] = useState(null);
+  const [nextLaunch, setNextLaunch] =useState<Launch[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [index, setIndex] = useState(1);
   useEffect(() => {
     async function loadLaunch() {
-      try {
-        setLoading(true);
-        const response = await fetch('https://ll.thespacedevs.com/2.3.0/launches/upcoming/');
-        
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        
-        const data = await response.json();
-        
-        // Set the next launch (first in the array)
-        if (data.results && data.results.length > 0) {
-          setNextLaunch(data.results);
-        }
-
-        setError(null);
-      } catch (error) {
-        console.error(error);
-        setError('Failed to load launch data');
-      } finally {
-        setLoading(false);
-      }
+    const launches = await FetchLaunch('https://ll.thespacedevs.com/2.3.0/launches/upcoming/',index);
+     setNextLaunch(launches);
+     console.log(launches.detail);
+       
     }
     
     loadLaunch();
   }, []);
+
+
 const handleValueChange = (value) => {
     setIndex(value);
+    loadLaunch()
   };
 
 if (nextLaunch != null){
      return (
     <>
       <div className='w-full h-[100vh]'>
-        <div className='z-0 absolute w-full h-full'>
-          <div className='w-full h-[80%] flex'>
-            <Rocket message={nextLaunch[index]} />
-            <Icon onValueChange={handleValueChange}/>
-          </div>
-          <Countdown launch={nextLaunch[index]} />
-        </div>
-        <Image 
-          src={nextLaunch[index].image.image_url}
-          width={6000} 
-          height={6000} 
-          className='w-full h-[100vh]' 
-          alt="background" />
-      </div> 
-      <div className='w-full h-[100vh]'>
-        <Description />
-      </div>
+              </div>
     </>
   );  
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,20 @@
 export function NextLaunch(index: number){
     if (index < 10) {
     index = index++
-
+  
     return index;
   }
-  else{
-    loadLaunch()
-  }
+
   return index;
 }
-
-
+// launches.ts
+export interface Launch {
+  id: string;
+  url: string;
+  name: string;
+}
+export async function FetchLaunch(url: string, index: number): Promise<Launch[]>{
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
